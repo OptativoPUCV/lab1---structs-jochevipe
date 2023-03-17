93c5e76bdbb071950b627578636fb6191ff5ff47
Replit user <>
2023-03-17 14:03:34 +0000

    new try

--- exercises.c
+++ exercises.c
@@ -92,7 +92,8 @@ Vector * crearVector(int n) {
   Vector *a = (Vector *) malloc(n*sizeof(Vector));
   if(a == NULL) exit(EXIT_FAILURE);
 
-   a->datos = (int*) calloc(n,sizeof(int));
+  a->datos = (int*) calloc(n,sizeof(int));
+  a->capacidad = n;
    return a;
 }
 
